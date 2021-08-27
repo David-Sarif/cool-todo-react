da6d57228f88677b7ebf9bd0ff653418abeeb418
David Sarif <you@example.comDavidSarif@yandex.ru>
2021-08-27 23:46:28 +0300

    feat: task fields are editable (for now)

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Tasks.scss';
 import AddTaskForm from './AddTaskForm';
 import editSvg from '../../assets/img/edit.svg'
@@ -7,7 +7,8 @@ import removeSvg from '../../assets/img/remove.svg'
 
 
 const Tasks = ({ list, onEditTitle, onAddTask, withoutEmpty }) => {
-    let [taskField, setTaskField] = useState('')
+    let [taskField, setTaskField] = useState(list.tasks)
+
     const editTitle = () => {
         const newTitle = window.prompt('Переназовите название списка', list.name);
         if (newTitle) {
@@ -15,13 +16,25 @@ const Tasks = ({ list, onEditTitle, onAddTask, withoutEmpty }) => {
         }
 
     }
-    const onEditTask = (id) => {
-        console.log('clicked, ', id)
+    useEffect(() => {
+        setTaskField(list.tasks)
+
+    }, [list])
+    const onTaskChange = (e, taskID) => {
+        const task = e.target.value;
+
+        const newTasks = taskField.map(el => { 
+            return el.id === taskID ? { ...el, text: task } : el})
+        
+        setTaskField(newTasks)
+    }
+    const allowEditForm = () => {
+
     }
 
     return (
         <div className="tasks">
-            <h2 style={{color:list.color.hex}} className="tasks__title">
+            <h2 style={{ color: list.color.hex }} className="tasks__title">
                 {list.name}
                 <img onClick={() => editTitle()} className='tasks__edit-icon' src={editSvg} alt="iconEdit" />
             </h2>
@@ -29,33 +42,38 @@ const Tasks = ({ list, onEditTitle, onAddTask, withoutEmpty }) => {
             <div className="tasks__items">
 
                 {!list.tasks && !withoutEmpty && <h2 className="tasks__empty">Задачи отсутствуют</h2>}
-                { list.tasks && list.tasks.map((task, id) => (
+                {list.tasks && list.tasks.map((task, id) => (
                     <div className="tasks__items-row" key={id}>
-                        <div className="checkbox">
+                        {taskField[id] &&  <>                            
 
-                            <input id={`task-${id}`} type="checkbox" name="" />
-                            <label htmlFor={`task-${id}`}>
-                                <svg width="11" height="8" viewBox="0 0 11 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001" stroke="#000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                                </svg>
-                            </label>
+                            <div className="checkbox">
 
-                        </div>
+                                <input id={`task-checkbox-${id}-${list.id}`} type="checkbox" name="" />
+                                <label htmlFor={`task-${id}`}>
+                                    <svg width="11" height="8" viewBox="0 0 11 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001" stroke="#000" strokeWidth="1.5"
+                                            strokeLinecap="round" strokeLinejoin="round" />
+                                    </svg>
+                                </label>
 
-                        <input value={task.text} onChange={()=>{}}/>
-                        <div className="tasks__items-row-actions">
-                            <div onClick={()=>onEditTask(id)}>
-                                <img src={editSvg} alt="edit_icon" />
                             </div>
-                            <div>
-                                <img src={removeSvg} alt="remove_icon" />
+                            <input value={taskField[id].text} onChange={(e) => onTaskChange(e,task.id)}
+                                id={`task-input-${id}-${list.id}`} />
+
+                            <div className="tasks__items-row-actions">
+                                <div onClick={()=>allowEditForm()}>
+                                    <img src={editSvg} alt="edit_icon" />
+                                </div>
+                                <div>
+                                    <img src={removeSvg} alt="remove_icon" />
+                                </div>
                             </div>
-                        </div>
+                        </>}
                     </div>))}
 
 
             </div>
-           <AddTaskForm list={list} onAddTask={onAddTask}/>
+            <AddTaskForm list={list} onAddTask={onAddTask} />
         </div>)
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import App from './App';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import './index.scss';
 
 ReactDOM.render(
 
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
   ,
   document.getElementById('root')
